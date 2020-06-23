262a079fba344b7435eb8e6b36cc734c22eef9f3
Thomas <thomas10191@hotmail.com>
2020-06-22 22:40:50 -0300

    Updates

---
+++ precache-manifest.75bb2e70217abf1b5f8d0eec8fea40d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "223bb0619237b63765076c0332509847",
+    "url": "/consuming-api-github/index.html"
+  },
+  {
+    "revision": "a907e0da4fd2d4e74912",
+    "url": "/consuming-api-github/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c64efab93dff534c11d4",
+    "url": "/consuming-api-github/static/js/2.ce627b69.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/consuming-api-github/static/js/2.ce627b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a907e0da4fd2d4e74912",
+    "url": "/consuming-api-github/static/js/main.22b9fe34.chunk.js"
+  },
+  {
+    "revision": "7168074c385173393366",
+    "url": "/consuming-api-github/static/js/runtime-main.eb637b49.js"
+  }
+]);
